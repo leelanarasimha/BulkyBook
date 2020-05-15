e470b4786aa5ed5c9d78b92c1556300fd916bcf6
leelanarasimha <leela.narsimha@gmail.com>
2020-05-15 23:50:42 +0530

    added category and cover type

---
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+       
+        public IActionResult Index()
+        {
+            var categories = _unitOfWork.Category.GetAll();
+
+            return View(categories);
+        }
+
+        public IActionResult Upsert(int? id) {
+
+            if (id == null)
+            {
+                var category = new Category();
+                return View(category);
+            }
+            else
+            {
+                var category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+                return View(category);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category) {
+            if (!ModelState.IsValid) {
+                return View(category);
+            }
+
+            if (category.Id == 0)
+            {
+                _unitOfWork.Category.Add(category);
+                
+            }
+            else
+            {
+                _unitOfWork.Category.Update(category);
+            }
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id) {
+            var category = _unitOfWork.Category.Get(id);
+            if (category == null) {
+                return NotFound();
+            }
+
+            _unitOfWork.Category.Remove(category);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll() {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+}
---
+++ BulkyBook/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var coverTypes = _unitOfWork.SP_Call.List<CoverType>(SD.Proc_CoverType_GetAll);
+            return View(coverTypes);
+        }
+
+        public IActionResult Upsert(int? id) {
+            var coverType = new CoverType();
+            
+            if (id != null) {
+                var parameter = new DynamicParameters();
+                parameter.Add("@Id", id);
+                coverType = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter);
+            }
+
+            return View(coverType);
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType coverType) {
+            if (!ModelState.IsValid) {
+                return View();
+            }
+
+            var parameter = new DynamicParameters();
+            parameter.Add("@Name", coverType.Name);
+            if (coverType.Id == 0) {
+                _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Create, parameter);
+            } else {
+                parameter.Add("@Id", coverType.Id);
+                _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Update, parameter);
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+            
+        }
+
+        public IActionResult Delete(int id) {
+            var parameters = new DynamicParameters();
+            parameters.Add("@Id", id);
+            var coverType = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameters);
+            if (coverType == null) {
+                return NotFound();
+            }
+
+            _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Delete, parameters);
+            _unitOfWork.Save();
+
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
